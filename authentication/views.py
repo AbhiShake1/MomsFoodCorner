c8379eb4feb493c36bb95be8f3ea3adeb94dec2e
abhi <abhishake1>
2022-05-17 18:29:07 +0545

    check if request is POST

--- authentication/views.py
+++ authentication/views.py
@@ -7,32 +7,36 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def signup(request: HttpRequest) -> JsonResponse:
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    try:
-        user: User = User.objects.create_user(username, username, password)
-        return JsonResponse({
-            'username': user.username,
-            'dateJoined': user.date_joined,
-            'isAdmin': user.is_superuser,
-        })
-    except:
-        return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
-
-
-@csrf_exempt
-def login(request: HttpRequest) -> JsonResponse:
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    try:
-        user = authenticate(username=username, password=password)
-        if user:
-            login(username=username, password=password)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user: User = User.objects.create_user(username, username, password)
             return JsonResponse({
                 'username': user.username,
                 'dateJoined': user.date_joined,
                 'isAdmin': user.is_superuser,
             })
-        return JsonResponse({'Error': 'Invalid Credentials'}, status=401)
-    except:
-        return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
+        except Exception as e:
+            return JsonResponse({'Error': str(e)}, status=401)
+    return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
+
+
+@csrf_exempt
+def login(request: HttpRequest) -> JsonResponse:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = authenticate(username=username, password=password)
+            if user:
+                login(username=username, password=password)
+                return JsonResponse({
+                    'username': user.username,
+                    'dateJoined': user.date_joined,
+                    'isAdmin': user.is_superuser,
+                })
+            return JsonResponse({'Error': 'Invalid Credentials'}, status=401)
+        except Exception as e:
+            return JsonResponse({'Error': str(e)}, status=401)
+    return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
