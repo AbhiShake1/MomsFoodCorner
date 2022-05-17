63b2e827aec908f61ae889b702d0176406997b77
abhi <abhishake1>
2022-05-17 17:32:39 +0545

    add signup

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
@@ -7,7 +8,13 @@ from django.views.decorators.csrf import csrf_exempt
 def signup(request: HttpRequest) -> JsonResponse:
     username = request.POST.get('username')
     password = request.POST.get('password')
-    return JsonResponse({
-        'username': username,
-        'password': password,
-    })
+    try:
+        user: User = User.objects.create_user(username, username, password)
+        return JsonResponse({
+            'username': user.username,
+            'dateJoined': user.date_joined,
+            'isAdmin': user.is_superuser,
+        })
+    except:
+        return JsonResponse({'Error': 'Only POST Allowed'}, status=401)
+
