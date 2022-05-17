e7e788d860cdf3a6f96fd24fe739e15cc364410f
abhi <abhishake1>
2022-05-17 18:07:02 +0545

    add login

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse, JsonResponse
 
@@ -16,5 +17,22 @@ def signup(request: HttpRequest) -> JsonResponse:
             'isAdmin': user.is_superuser,
         })
     except:
-        return JsonResponse({'Error': 'Only POST Allowed'}, status=401)
+        return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
 
+
+@csrf_exempt
+def login(request: HttpRequest) -> JsonResponse:
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    try:
+        user = authenticate(username=username, password=password)
+        if user:
+            login(username=username, password=password)
+            return JsonResponse({
+                'username': user.username,
+                'dateJoined': user.date_joined,
+                'isAdmin': user.is_superuser,
+            })
+        return JsonResponse({'Error': 'Invalid Credentials'}, status=401)
+    except:
+        return JsonResponse({'Error': 'Only POST Allowed'}, status=403)
