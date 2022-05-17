7cc5ae3f632c70389780967a198007760ae94e28
abhi <abhishake1>
2022-05-17 16:00:26 +0545

    allow all hosts

--- MomsFoodCorner/settings.py
+++ MomsFoodCorner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o96*m7!#ff==_)55jkk&438=4jd+q8f!%l#p565eq97is-#u$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
